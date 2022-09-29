be2ac2fdea9723fb1bb6cc50037a6fe7c2aee3df
Harshit Bansal <harshitbansal394@gmail.com>
2022-09-29 21:24:28 +0530

    add exception classes to day3 task

---
+++ exceptions/AccountantException.java
@@ -0,0 +1,10 @@
+package com.masai.exceptions;
+
+public class AccountantException extends Exception{
+public AccountantException() {
+	super();
+}
+public AccountantException(String error) {
+	super(error);
+}
+}
